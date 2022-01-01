8be7d865ad9f9df4bfd700cda1b29af2857186e2
erfanpaya <erfanpaya2021@gmail.com>
2022-01-01 21:10:01 +0330

    get coins data with axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-function App() {
-  
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-  return <div className="App"></div>;
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    getCoins();
+  }, []);
+
+  const getCoins = () => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+      )
+      .then((data) => setCoins(data.data))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
