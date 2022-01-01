abd1a41342823d38cf84107c57d189f4994334f0
erfanpaya <erfanpaya2021@gmail.com>
2022-01-01 22:32:18 +0330

    create coin component - style it - show it

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function App() {
+import "./App.css";
+import Coin from "./components/Coin";
+
+const App = () => {
+  // === STATES ===
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getCoins();
+    refreshPage();
   }, []);
 
-  const getCoins = () => {
+  // === GET COINS ===
+  const refreshPage = () => {
+    setIsLoading(true);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
       )
-      .then((data) => setCoins(data.data))
-      .catch((err) => console.log(err));
+      .then((response) => {
+        setIsLoading(false);
+        setCoins(response.data);
+      });
   };
+
+  // === COIN ELEMENTS ===
+  const coinsElements = coins.map((coin) => {
+    return (
+      <Coin
+        key={coin.id}
+        id={coin.id}
+        icon={coin.image}
+        coinName={coin.name}
+        coinSymbol={coin.symbol}
+        price={coin.current_price}
+        priceChange={coin.price_change_percentage_24h}
+        marketCap={coin.market_cap}
+      />
+    );
+  });
 
   return (
     <div className="App">
+      <header className="header">
+        <h2>Welcome to the CryptoChecker</h2>
+      </header>
+      <div className="coins-wrapper">
+        {isLoading ? (
+          <h2 className="loadingMssg">Data Loading...</h2>
+        ) : (
+          coinsElements
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Coin.js
@@ -0,0 +1,32 @@
+import "./Coin.css";
+
+const Coin = ({
+  icon,
+  coinName,
+  coinSymbol,
+  price,
+  priceChange,
+  marketCap,
+}) => {
+  return (
+    <div className="coin">
+      <img className="coin-image" src={icon} alt="img" />
+      <h3 className="coin-name">{coinName}</h3>
+      <p className="coin-symbol">{coinSymbol}</p>
+      <p className="coin-price">$ {price.toFixed(2)}</p>
+      {priceChange > 0 ? (
+        <p className="coin-price-change" style={{ color: "green" }}>
+          +{priceChange.toFixed(2)} %
+        </p>
+      ) : (
+        <p className="coin-price-change" style={{ color: "red" }}>
+          {priceChange.toFixed(2)} %
+        </p>
+      )}
+      <p className="coin-marketcap">$ {marketCap.toLocaleString()}</p>
+      <button className="coin-info-btn">More Info</button>
+    </div>
+  );
+};
+
+export default Coin;
